f60e657d07cc6b7ec40b5772be58455c45c8218d
karuniamalik <karuniamalik@gmail.com>
2024-02-01 20:20:37 +0700

    tambah view bay produk

--- resources/views/pages/dashboard-transaction.blade.php
+++ resources/views/pages/dashboard-transaction.blade.php
@@ -139,6 +139,45 @@
                                    
                       </div>
                     </div>
+                        <div
+              class="tab-pane fade"
+              id="pills-profile"
+              role="tabpanel"
+              aria-labelledby="pills-profile-tab"
+            >
+              @foreach ($buyTransactions as $transaction)
+                  <a
+                    href="{{ route('dashboard-transaction-details', $transaction->id) }}"
+                    class="card card-list d-block"
+                  >
+                    <div class="card-body">
+                      <div class="row">
+                        <div class="col-md-1">
+                          <img
+                            src="{{ Storage::url($transaction->product->galleries->first()->photos ?? '') }}"
+                            class="w-50"
+                          />
+                        </div>
+                        <div class="col-md-4">
+                          {{ $transaction->product->name }}
+                        </div>
+                        <div class="col-md-3">
+                          {{ $transaction->product->user->store_name }}
+                        </div>
+                        <div class="col-md-3">
+                          {{ $transaction->created_at }}
+                        </div>
+                        <div class="col-md-1 d-none d-md-block">
+                          <img
+                            src="/images/dashboard-arrow-right.svg"
+                            alt=""
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </a>
+              @endforeach
+            </div>
                 </div>
               
             </div>
